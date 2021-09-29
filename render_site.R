501a88a109bf2ef81fed003cf6cdadb3b62173e5
Ben Best <ben@ecoquants.com>
2021-09-29 09:31:52 -0700

    + Human Dimensions, Ocean & Climate scenes, svg, nav menus

--- render_site.R
+++ render_site.R
@@ -3,8 +3,8 @@ source("infographiqR.R")
 # update site ----
 
 gsheets_to_csvs(gsheet)
-create_svg_links()
-add_gimage_paths() # add image_path to data/gsheets/figures.csv
+# add_gimage_paths() # add image_path to data/gsheets/figures.csv
+# create_svg_links()
 
 create_modals()
 render_modals()
