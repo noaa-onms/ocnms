5274f098b0f13125e37408adb0ad04ff1d43c0e7
Ben Best <ben@ecoquants.com>
2021-09-29 11:03:53 -0700

    regen w/ new _modal templates; fixes #6

--- render_site.R
+++ render_site.R
@@ -2,11 +2,11 @@ source("infographiqR.R")
 
 # update site ----
 
-gsheets_to_csvs(gsheet)
+# gsheets_to_csvs(gsheet)
 # add_gimage_paths() # add image_path to data/gsheets/figures.csv
 # create_svg_links()
 
-# TODO: + arg for only_new = T
+# TODO: + arg for only_new = T, eg for rocky-shore MARINe versions being interactive & diff't
 create_modals()
 render_modals()
 
